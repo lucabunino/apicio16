505392c94573bfaef256661dfd0e697e05446fcf
lucabunino <66553738+lucabunino@users.noreply.github.com>
2024-03-14 17:11:13 +0100

    Update svelte.config.js

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -2,6 +2,7 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-vercel';
 
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
